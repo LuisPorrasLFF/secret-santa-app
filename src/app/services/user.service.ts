4bd218f630589a314649b2beb04d15cc65b27d44
Luis.Porras <luis.porras@ascendlearning.com>
2024-01-03 15:13:14 -0600

    Replace Material table with regulat table

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -28,6 +28,6 @@ export class UserService {
 
   removeUser(user: User){
     let userIndex = this.users.findIndex(u => u.id == user.id);
-    this.users.splice(userIndex);
+    this.users.splice(userIndex, 1);
   }
 }
--- src/app/user-components/user-list/user-list.component.ts
+++ src/app/user-components/user-list/user-list.component.ts
@@ -1,19 +1,28 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatTableModule } from '@angular/material/table';
 import { UserService } from '../../services/user.service';
 
+import { User } from './../../models/users';
+
 @Component({
   selector: 'app-user-list',
   standalone: true,
-  imports: [MatButtonModule, MatIconModule, MatTableModule],
+  imports: [CommonModule, MatButtonModule, MatIconModule, MatTableModule],
   templateUrl: './user-list.component.html',
   styleUrl: './user-list.component.css'
 })
-export class UserListComponent {
-  users = this.userService.getUsers();
-  displayedColumns: string[] = ['id', 'name', 'email'];
+export class UserListComponent{
+  displayedColumns: string[] = ['id', 'name', 'email', 'edit', 'delete'];
+
+  users: User[] = this.userService.getUsers();
+
+  deleteUser(user: User): void {
+    this.userService.removeUser(user);
+    console.log(this.users);
+  }
 
   constructor(
     private userService: UserService
